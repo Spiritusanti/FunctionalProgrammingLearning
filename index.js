84c1839592e1c4a7f7665bad6781a3b9fd59a529
Jacob McCracken <mccrackenjacob@gmail.com>
2020-09-04 18:56:01 +0000

    added quick examples of pure functions

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // learning about functional programming!
+
+// first off: pure functions!
+// always return the same output given the same input
+// Cannot modify anything outside of itself. No side effects!
+
+const array = [1, 2, 3]
+// function mutateArray(arr) {
+//   arr.pop()
+// }
+
+// function mutateArray2(arr) {
+//   arr.forEach(item => {
+//     arr.push(1)
+//   })
+// }
+
+// this is not a pure function because it affects objects in the global space.
+// mutateArray(array);
+// mutateArray2(array);
+// console.log(array)
+
+// So how do we write something with no side effects?
+
+// by creating a new array within the function we eliminate side effects
+function removeLastItem(arr) {
+  const newArray = [].concat(arr);
+  newArray.pop()
+  return newArray
+}
+
+function multiplyBy2(arr) {
+  return arr.map(item => item*2)
+}
+const array2 = removeLastItem(array);
+const array3 = multiplyBy2(array);
+console.log(array, array2, array3);
+// now we have different arrays living on their own with no side effects!
+
+// referential transparency
+// -> as long as the input is the same the output will be the same!
+
+
+// 
+
